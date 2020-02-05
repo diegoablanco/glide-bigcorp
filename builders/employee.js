8a8644f1cbf4c7689a58c7a658128995d7c8584f
Diego <diegoablanco@gmail.com>
2020-02-05 11:57:53 -0300

    fix error condition

--- builders/employee.js
+++ builders/employee.js
@@ -2,7 +2,7 @@ const employeeProvider = require('../providers/employee')
 async function build (employee, expands) {
     const managerExpands = expands.filter(expand => expand.indexOf('manager') == 0)
     const departmentExpands = expands.filter(expand => expand.indexOf('department') == 0)
-    if(managerExpands.length > 0 && employee.manager) {
+    if(managerExpands.length > 0 && typeof(employee.manager) === "number") {
         employee.manager = await employeeProvider.getById(employee.manager, managerExpands.map(expand => expand.slice(1)))
     }
     return employee
